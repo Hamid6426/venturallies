019cb6f1eda2dc9c5a8b9dccfe23d1aaf8bf2869
Mian Hamid Ur Rehman <mianhamid6426@gmail.com>
2025-06-03 07:47:38 -0700

    angular setup

---
+++ angular/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then((m) => m.About),
+  },
+];
---
+++ angular/src/app/components/index.ts
@@ -0,0 +1,5 @@
+// src/app/components/index.ts
+// This is called barrel files for flat imports
+export * from './navbar/navbar';
+export * from './button/button';
+
