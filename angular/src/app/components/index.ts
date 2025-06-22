2833fac71ef173259b8f2c4de8bb13cd3ff21013
Mian Hamid Ur Rehman <mianhamid6426@gmail.com>
2025-06-22 10:20:58 +0500

    deleted other folders

--- angular/src/app/components/index.ts
+++
@@ -1,5 +0,0 @@
-// src/app/components/index.ts
-// This is called barrel files for flat imports
-export * from './navbar/navbar';
-export * from './button/button';
-
--- express-ts/src/controllers/user.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import prisma from "../configs/prisma";
-
-export const getUsers = async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-};
